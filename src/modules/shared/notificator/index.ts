48c3abd1b01b275f4e82479d486398098d7437ee
michee <pawindtaore.nangma-salam@digital.gouv.tg>
2024-10-23 15:15:19 +0000

    feat: implement parallel OTP sending via SMS and email

--- src/modules/shared/notificator/index.ts
+++ src/modules/shared/notificator/index.ts
@@ -5,3 +5,28 @@ export const Notificator = {
   mail: MailServiceUtilities,
   sms: SmsServiceUtilities,
 };
+
+export class NotifyByOtp {
+  static async send({
+    to,
+    code,
+    purpose,
+  }: {
+    to: string;
+    code: string;
+    purpose: string;
+  }): Promise<void> {
+    try {
+      await Promise.all([
+        Notificator.sms.sendSmsOtp({ to, code }),
+        Notificator.mail.sendMailOtp({ to, code, purpose }),
+      ]);
+
+      LOGGER.info(
+        'Envoie des notifications sur tous les canaux reussi avec succes',
+      );
+    } catch (error) {
+      LOGGER.error("Erreur lors de l'envoi des notifications: ", error);
+    }
+  }
+}
